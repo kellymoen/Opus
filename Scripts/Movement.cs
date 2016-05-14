88d0643b95593e9abfcd41848878bec5dec86559
Paige-H <paige@paratu.net>
2016-05-14 12:08:09 +1200

    Tidied up movement code

--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -8,17 +8,18 @@ public class Movement : MonoBehaviour {
 	public float gravity = 20.0F;
 	public Transform spawnPoint;
 
+	private CharacterController controller;
 	private Animator animator;
 	private Vector3 moveDirection = Vector3.zero;
 	private Camera camera;
 
 	void Start(){
+		controller = GetComponent<CharacterController>();
 		  camera = Camera.main;
 		animator = GetComponent<Animator>();
 	}
 
 	void Update() {
-		CharacterController controller = GetComponent<CharacterController>();
 		if (controller.isGrounded) {
 			if(Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Vertical") > 0){
 				animator.SetBool("isWalking", true);
