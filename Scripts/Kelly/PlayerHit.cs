f6f26e8cb31da3277cd8f89d0b6a7c11499b08eb
kellymoen <kel.moen@gmail.com>
2016-05-18 00:25:09 +1200

    Added a 30ms delay and slightly more generous timing windows

--- Scripts/Kelly/PlayerHit.cs
+++ Scripts/Kelly/PlayerHit.cs
@@ -15,18 +15,19 @@ public class PlayerHit : MonoBehaviour {
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.X)) {
 			double diff = Time.time - metronome.GetBeatStartTime ();
+			diff -= 0.03; // 30ms delay, 
 			if (diff > 0.2728) //greater than a half beat means the player is likely early
 				diff -= 0.5455;
 			Debug.Log (diff);
-			if (diff < -0.08)
+			if (diff < -0.1)
 				display.text = "BAD: EARLY";
-			else if (diff < -0.02)
+			else if (diff < -0.03)
 				display.text = "GOOD: EARLY";
 			else if (diff < 0)
 				display.text = "GREAT: EARLY";
-			else if (diff > 0.08)
+			else if (diff > 0.1)
 				display.text = "BAD: LATE";
-			else if (diff > 0.02)
+			else if (diff > 0.03)
 				display.text = "GOOD: LATE";
 			else if (diff > 0.00)
 				display.text = "GREAT: LATE";
