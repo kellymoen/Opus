a706a090ed88bffc590903b7f0a83692c4fdf869
Paige-H <paige@paratu.net>
2016-05-25 17:03:26 +1200

    Fixed Camera and updated Player Prefab

--- Assets/Scripts/CameraLook.cs
+++ Assets/Scripts/CameraLook.cs
@@ -4,17 +4,11 @@ using UnityStandardAssets.CrossPlatformInput;
 public class CameraLook : MonoBehaviour {
 	private GameObject target;
 	Vector3 offset;
-	public float mouseSensitivity = 70f;
-	private float xOffset = 0;
-	private float yOffset = 0;
-
-	public float MinimumY = 0F;
-	public float MaximumY = 180F;
-	public float damping = .5f;
+	public float turnSpeed = 2.0f;
+	public float damping = 1f;
 
 
 	void Start(){
-
 		Cursor.visible = false;
 		Cursor.lockState = CursorLockMode.Locked;
 		target = GameObject.FindWithTag ("Player");
@@ -22,25 +16,11 @@ public class CameraLook : MonoBehaviour {
 		transform.LookAt(target.transform.position);
 	}
 
-	void LateUpdate() {
-		//follow player
-		Vector3 desiredPosition = target.transform.position + offset;
-		Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
-		transform.position = position;
-		//Rotate according to mouse position
-		updateMouseRotation();
-		//float y = clampFloat(transform.position.y + yOffset, MinimumY, MaximumY);
-		Vector3 desiredRotPosition = new Vector3(transform.position.x + xOffset, transform.position.y + yOffset, transform.position.z);
-		transform.RotateAround(target.transform.position, desiredRotPosition, mouseSensitivity * Time.deltaTime);
-		transform.LookAt(target.transform.position);
+	void LateUpdate(){
+		offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+    transform.position = target.transform.position + offset;
+    transform.LookAt(target.transform.position);
 		checkEsc();
-	}
-
-	void updateMouseRotation(){
-		float mouseX = CrossPlatformInputManager.GetAxis("Mouse X");
-		float mouseY = CrossPlatformInputManager.GetAxis("Mouse Y");
-		yOffset += mouseX *  Time.deltaTime;
-		xOffset += mouseY *  Time.deltaTime;
 	}
 
 	void checkEsc(){
@@ -48,16 +28,6 @@ public class CameraLook : MonoBehaviour {
 			Cursor.lockState = CursorLockMode.None;
 			Cursor.visible = true;
 		}
-	}
-
-	float clampFloat(float num, float min, float max){
-		if(num < min){
-			return min;
-		}
-		if(num > max){
-			return max;
-		}
-		return num;
 	}
 
 }
