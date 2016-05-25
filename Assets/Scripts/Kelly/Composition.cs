0acadbf16c47e9ff56abf447161256542ec92540
kellymoen <kel.moen@gmail.com>
2016-05-25 21:30:40 +1200

    Composition system working!

--- Assets/Scripts/Kelly/Composition.cs
+++ Assets/Scripts/Kelly/Composition.cs
@@ -7,21 +7,25 @@ public class Composition : MonoBehaviour
 	public AudioSource audioSource;
 	public AudioClip sound;
 	public AudioSourceMetro metro;
+	public Circle circle;
+	public GameObject noteSprite;
 
 	public int[] bars = {1,0,1,0};
 	public int numBars = 2;
 	float currentBar;
 	double wait;
+	double currentTime;
+	double totalTime;
 	public bool mute = false;
 
 	public void Start ()
 	{
-		audioSource=this.GetComponent<AudioSource>();
-		wait = metro.GetNextBarStartTime();
+		currentTime = 0;
+		totalTime = numBars * 4 * metro.BEAT_TIME * bars.Length;
 	}
 
 	void Update(){
-		int barIndex = (metro.GetBarCounter () / numBars) % bars.Length;
+		int barIndex = (metro.GetBarCounter() / numBars) % bars.Length;
 		if (bars[barIndex] == 1) {
 			mute = true;
 		} else {
@@ -32,6 +36,10 @@ public class Composition : MonoBehaviour
 		} else {
 			audioSource.volume = 1;
 		}
+		currentTime += metro.GetDeltaTime();
+
+
+		noteSprite.transform.position = circle.PositionOnCircle(currentTime/totalTime);
 	}
 }
 
