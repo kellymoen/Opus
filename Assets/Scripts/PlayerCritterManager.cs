7044fe0c14b906b5600af75c48998c2218c7c638
Paige-H <paige@paratu.net>
2016-06-13 13:46:02 +1200

    Started critter manager and composition controller intergration

--- Assets/Scripts/PlayerCritterManager.cs
+++ Assets/Scripts/PlayerCritterManager.cs
@@ -4,11 +4,14 @@ using System;
 
 public class PlayerCritterManager : MonoBehaviour {
   private Dictionary<Transform, GameObject> activeCritters;
+  private Dictionary<Composition, GameObject> compositionsCritters;
   //all available critters
   private List<GameObject> allCritters;
   private float heightOffGround = 0.5f;//For moving critters around, to avoid getting stuck add extra height
+  private CompositionController compController;
 
   void Start(){
+    compController = GameObject.Find("CompositionController").GetComponent<CompositionController>();
     activeCritters = new Dictionary<Transform, GameObject>();
     allCritters = new List<GameObject>();
     foreach (Transform child in GameObject.Find("CritterSlotContainer").transform)
@@ -29,6 +32,11 @@ public class PlayerCritterManager : MonoBehaviour {
     }
     //add to List of all critters
     allCritters.Add(critter);
+    //get the composition script attached to critter
+    Composition added = compController.AddNoot(critter);
+    if(added != null){
+      compositionsCritters.Add(added, critter);
+    }
   }
 
   public bool addCritterToFirstSpaceAvailable(GameObject critter){
@@ -42,11 +50,27 @@ public class PlayerCritterManager : MonoBehaviour {
     }
     return false;
   }
-
-  public bool stopCritterFollowing(GameObject critter){
-    if(critter == null){
+  public bool startCritterFollowing(Composition composition){
+    if(composition == null){
       return false;
     }
+    GameObject critter = compositionsCritters[composition];
+    foreach(KeyValuePair<Transform, GameObject> pair in activeCritters){
+      if(pair.Value == null){
+        activeCritters[pair.Key] = critter;
+        critter.SetActive(true);
+        critter.transform.position = new Vector3(pair.Key.position.x, pair.Key.position.y + heightOffGround, pair.Key.position.z);
+        critter.GetComponent<AIManagerScript>().startFollowing(pair.Key);
+        return true;
+      }
+    }
+    return false;
+  }
+  public bool stopCritterFollowing(Composition composition){
+    if(composition == null){
+      return false;
+    }
+    GameObject critter = compositionsCritters[composition];
     foreach(KeyValuePair<Transform, GameObject> pair in activeCritters){
       if(pair.Value == critter){
         activeCritters[pair.Key] = null;
